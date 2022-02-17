47c438363850ae773b7047ff72f4818950f05e93
Mack Maier <hmaiermack@gmail.com>
2022-02-17 10:30:41 -0800

    auth routes and corresponding services init

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  //local: username/password auth, not implementing third party signup eg google
+  @Post('/local/signup')
+  signUpLocal() {
+    this.authService.signUpLocal();
+  }
+
+  @Post('/local/signin')
+  signInLocal() {
+    this.authService.signInLocal();
+  }
+
+  @Post('/logout')
+  logout() {
+    this.authService.logout();
+  }
+
+  @Post('/refresh')
+  refreshTokens() {
+    this.authService.refreshTokens();
+  }
+}
