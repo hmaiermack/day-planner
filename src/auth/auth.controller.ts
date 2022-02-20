d51e51b544ffa07b797a97c7af94faec20418d21
Mack Maier <hmaiermack@gmail.com>
2022-02-20 12:07:42 -0800

    signup-local dto, prisma migration fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   //local: username/password auth, not implementing third party signup eg google
   @Post('/local/signup')
-  signUpLocal() {
-    this.authService.signUpLocal();
+  signUpLocal(@Body() dto: AuthDto) {
+    this.authService.signUpLocal(dto);
   }
 
   @Post('/local/signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signUpLocal() {}
+  signUpLocal(dto: AuthDto) {
+    //   const newUser = this.prisma.user.create({
+    //       data: {
+    //           email: dto.email
+    //       }
+    //   })
+
+    const { email, password } = dto;
+
+    this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
 
   signInLocal() {}
 
