e693b01b7bf5f12c724e15464e67c49c98f82ad1
Mack Maier <hmaiermack@gmail.com>
2022-02-21 12:49:36 -0800

    logout function and jwt guard on auth/logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { Tokens } from './types';
@@ -19,10 +29,12 @@ export class AuthController {
     return this.authService.signInLocal(dto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Post('/logout')
   @HttpCode(HttpStatus.OK)
-  logout() {
-    this.authService.logout();
+  logout(@Req() req: Request) {
+    const user = req.user;
+    return this.authService.logout(user['sub']);
   }
 
   @Post('/refresh')
--- src/auth/strategies/access_token.strategy.ts
+++ src/auth/strategies/access_token.strategy.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 
+type JwtPayload = {
+  sub: string;
+  email: string;
+};
+
 @Injectable()
 export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
