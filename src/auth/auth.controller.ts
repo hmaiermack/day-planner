172c95c9545c46828d6da8d9e1209d7c109e07f3
Mack Maier <hmaiermack@gmail.com>
2022-02-21 12:34:57 -0800

    Http status codes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { Tokens } from './types';
@@ -8,21 +8,25 @@ export class AuthController {
   constructor(private authService: AuthService) {}
   //local: username/password auth, not implementing third party signup eg google
   @Post('/local/signup')
+  @HttpCode(HttpStatus.CREATED)
   signUpLocal(@Body() dto: AuthDto): Promise<Tokens> {
     return this.authService.signUpLocal(dto);
   }
 
   @Post('/local/signin')
+  @HttpCode(HttpStatus.OK)
   signInLocal(@Body() dto: AuthDto): Promise<Tokens> {
     return this.authService.signInLocal(dto);
   }
 
   @Post('/logout')
+  @HttpCode(HttpStatus.OK)
   logout() {
     this.authService.logout();
   }
 
   @Post('/refresh')
+  @HttpCode(HttpStatus.OK)
   refreshTokens() {
     this.authService.refreshTokens();
   }
