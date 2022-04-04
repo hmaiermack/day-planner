ed095b7115a0d288f755e823047a30bcbd5b0b55
Mack Maier <mack@Macks-MacBook-Air.local>
2022-04-04 15:45:26 -0700

    create habit service

--- api/src/habits/habits.service.ts
+++ api/src/habits/habits.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { Habit } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { NewHabitDto } from './dto/newHabit.dto';
 
 @Injectable()
-export class HabitsService {}
+export class HabitsService {
+    constructor(private prisma: PrismaService) {}
+
+    async createHabit(userId: number, dto: NewHabitDto): Promise<Habit> {
+        
+        
+        if(dto.remindTime) {
+            const habit = await this.prisma.habit.create({
+                data: {
+                    user: {
+                        connect: {
+                            id: userId
+                        }
+                    },
+                    name: dto.name,
+                    habitDays: dto.habitDays,
+                    remindTime: dto.remindTime,
+                    remindDays: dto.remindDays
+                }
+            })
+
+            return habit
+        } else {
+            const habit = await this.prisma.habit.create({
+                data: {
+                    user: {
+                        connect: {
+                            id: userId
+                        }
+                    },
+                    name: dto.name,
+                    habitDays: dto.habitDays,
+                }
+
+            })
+
+            return habit
+        }
+    }
+}
